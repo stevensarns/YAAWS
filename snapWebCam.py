8eee8e9d94d19596a68cdf11e632df976eca903b
Steven Sarns <stevensarns@gmail.com>
2023-03-25 14:55:46 -0700

    Create snapWebCam.py

---
+++ snapWebCam.py
@@ -0,0 +1,17 @@
+
+import time
+import datetime as dt
+
+
+fswebcam -save, test.jpg -d, "qucik cam pro 9000"
+
+#camera.resolution = (640, 480)
+# camera.annotate_background = Color('black')
+# camera.annotate_foreground = Color('white')
+# camera.annotate_text_size = 15 # (values 6 to 160, default is 32)
+# camera.annotate_text = dt.datetime.now().strftime('%A %I:%M %p')
+# time.sleep(2)
+# camera.capture("/home/steven/Scripts/pic.jpg")
+# time.sleep(2)
+# camera.close()
+                
